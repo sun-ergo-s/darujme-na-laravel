1abcb90d00d2b44b0d091865fad68b2dab120fd6
Michal <mvesely26@gmail.com>
2024-07-22 21:56:01 +0200

    config file on boot

--- src/DarujmeServiceProvider.php
+++ src/DarujmeServiceProvider.php
@@ -7,10 +7,24 @@ use SunErgoS\DarujmeNaLaravel\DarujmeNaLaravel;
 
 class DarujmeServiceProvider extends ServiceProvider {
 
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/darujme.php' => $this->app->configPath('darujme.php'),
+        ]);
+    }
+
     public function register(): void
     {
 
-        $this->mergeConfigFrom(config_path() . '/darujme.php', 'darujme');
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/darujme.php', 'darujme'
+        );
 
         $this->app->singleton('darujme', function() {
             return new DarujmeNaLaravel();
